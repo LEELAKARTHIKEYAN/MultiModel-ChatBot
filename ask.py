682414204f30c0faadebb3bb92fb5712e32f0d83
unknown <leelakarthikeyan13@gmail.com>
2024-11-29 19:47:21 +0530

    29th nov update

--- ask.py
+++
@@ -1,6 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyBxqpDDdWR0EmIYfXxpXADx5W9WXVm_xLc")
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Explain how AI works")
-print(response.text)
